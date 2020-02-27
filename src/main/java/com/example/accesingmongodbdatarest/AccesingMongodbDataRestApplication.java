21e192fddf352338f2fcdc8eadb179c5911ef6b9
Raul Lucas <raul_lucas@hotmail.com>
2020-02-27 18:27:12 +0000

    Initial commit

---
+++ src/main/java/com/example/accesingmongodbdatarest/AccesingMongodbDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.examplo.accesingmongodbdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesingMongodbDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccesingMongodbDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examplo/accesingmongodbdatarest/AccesingMongodbDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplo.accesingmongodbdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesingMongodbDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
